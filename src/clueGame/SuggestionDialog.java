c9d3227160d1bf7581a79ccdefc7e2c2a0460a51
Blake Adauto <badauto@mines.edu>
2022-11-26 23:27:23 -0700

    Added JComboBoxes for weapons and people for user to make suggestions with

--- src/clueGame/SuggestionDialog.java
+++ src/clueGame/SuggestionDialog.java
@@ -5,40 +5,83 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class SuggestionDialog extends JDialog
 {
-	private JTextField tempText;
-	private JTextField tempDisplay;
+	private JPanel leftPanel;
+	private JPanel rightPanel;
+	
+	private JTextField curRoom;
+	private JComboBox<String> weapons;
+	private JComboBox<String> people;
 	
 	public SuggestionDialog()
 	{
 		setTitle("Make a suggestion");
-		
-		setSize(400, 400);
-		
-		setLayout(new GridLayout(2, 2));
-		
-		tempText = new JTextField(20);
-		tempDisplay = new JTextField(20);
-		
-		JButton okButton = new JButton("Submit");
-		okButton.addActionListener(new ActionListener()
+		setSize(300, 200);
+		setLayout(new GridLayout(0, 2));
+				
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent press) 
 			{
-				tempDisplay.setText(tempText.getText());;
+				System.out.println(curRoom.getText());
+				System.out.println(people.getSelectedItem());
+				System.out.println(weapons.getSelectedItem());
+				//Submit the suggestion to the game
+				
+				setVisible(false);
 				
 			} //end actionPerformed
 			
 		}); //end addActionListener
 		
-		add(okButton);
-		add(tempText);
-		add(tempDisplay);
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent press) 
+			{
+				setVisible(false);
+				
+			} //end actionPerformed
+			
+		}); //end addActionListener
+		
+		leftPanel = new JPanel();
+		leftPanel.setLayout(new GridLayout(4, 1));
+		leftPanel.add(new JLabel("Current Room"));
+		leftPanel.add(new JLabel("Person"));
+		leftPanel.add(new JLabel("Weapon"));
+		leftPanel.add(submit);
+		
+		curRoom = new JTextField("Room...");
+		curRoom.setEditable(false);
+		
+		weapons = new JComboBox<String>();
+		weapons.addItem("Weapon 1");
+		weapons.addItem("Weapon 2");
+		
+		people = new JComboBox<String>();
+		people.addItem("Person 1");
+		people.addItem("Person 2");
+		
+		rightPanel = new JPanel();
+		rightPanel.setLayout(new GridLayout(4, 1));
+		rightPanel.add(curRoom);
+		rightPanel.add(people);
+		rightPanel.add(weapons);
+		rightPanel.add(cancel);
+		
+		add(leftPanel);
+		add(rightPanel);
 		
 		setVisible(true);
 		
